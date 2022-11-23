c4ad53ace9d2cd03517d1d32143d98701cc8c906
manorajesh <mano@raje.sh>
2022-11-22 22:29:35 -1000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+// __ ADD
+// track file
+// check file diff
+// commit changes to delta file
+
+// __ REVERT
+// check file diff
+// change file to previous version (opposite operation)
+
+use clap::{Parser, Subcommand};
+use std::{fs::{self, create_dir, File}, io::{Write, Read}};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Subcommand)]
+enum SubCommand{
+    Status,
+    Add {
+        file: String,
+    },
+    Knot {
+        #[clap(short, long)]
+        message: Option<String>,
+    },
+    Init,
+
+}
+fn main() {
+    let cli = Cli::parse();
+
+    match cli.subcmd {
+        SubCommand::Init => {
+            create_dir(".rift").expect("Failed to create .rift directory");
+            create_dir(".rift/deltas").expect("Failed to create .rift/deltas directory"); // changes to files
+            File::create(".rift/flagship").expect("Failed to create .rift/flagship file"); // list of files to track
+            println!("Initialized empty rift repository in .rift");
+        }
+        SubCommand::Add { file } => {
+            let file_name = file.clone();
+            let mut tracked_files = File::create(".rift/flagship").expect("Failed to open .rift/flagship file\nRun `rift init` to initialize a rift repository");
+            
+            if fs::read_to_string(".rift/flagship").unwrap().contains(&file_name) {
+                println!("File already tracked");
+            } else {
+                tracked_files.write((file + "\n").as_bytes()).expect("Failed to write to .rift/flagship file");
+                println!("tracking {}", file_name);
+            }
+        },
+        SubCommand::Knot { message } => {
+            unimplemented!()
+        },
+        SubCommand::Status => {
+            unimplemented!()
+        },
+    }
+}
